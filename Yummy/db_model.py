95f67a86a2fa4d244c2117014f8fb6838c6559e3
juahuer1 <juahuer1@teleco.upv.es>
2025-03-18 17:58:20 +0100

    define main tables for app main functions

--- Yummy/db_model.py
+++ Yummy/db_model.py
@@ -6,7 +6,33 @@ class User(rx.Model, table=True):
     password: str
     imagepath: str | None
 
+class Receta(rx.Model, table=True):
+    """Tabla de recetas"""
+    nombre: str
+    variante: str
+    creador: str
+
 class Ingrediente(rx.Model, table=True):
     """Tabla de ingredientes para las recetas"""
     nombre: str
     variante: str | None
+
+class Ingrediente_Receta(rx.Model, table = True):
+    """Relacion entre recetas e ingredientes"""
+    id_receta: int
+    id_ingrediente: int
+    cantidad: int
+    unidad: str
+
+class Pasos_Receta(rx.Model, table=True):
+    """Tabla de pasos para las recetas"""
+    id_receta: int
+    numero_paso: int
+    descripcion: str
+
+class Imagen_Receta(rx.Model, table=True):
+    """Tabla de imagenes para las recetas"""
+    id_receta: int
+    id_paso: int
+    image_path: str
+    descripcion: str
