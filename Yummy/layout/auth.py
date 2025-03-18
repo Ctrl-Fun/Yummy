8d5f281f7483485a822d7d8e85cf577c343bbb46
juahuer1 <juahuer1@teleco.upv.es>
2025-03-18 16:40:19 +0100

    cambios en estilos login

--- Yummy/layout/auth.py
+++ Yummy/layout/auth.py
@@ -1,32 +1,37 @@
 """Shared auth layout."""
 
 import reflex as rx
+from Yummy.styles import styles
 
 def auth_layout(*args):
     """The shared layout for the login and sign up pages."""
     return rx.box(
-            rx.vstack(
-                rx.heading("Welcome to PySocial!", size="8"),
-                rx.heading("Sign in or sign up to get started.", size="8"),
-                align="center",
-            ),
-            rx.text(
-                "See the source code for this demo app ",
-                rx.link(
-                    "here",
-                    href="https://github.com/reflex-dev/reflex-examples/tree/main/twitter",
+            rx.center(
+                rx.vstack(
+                    rx.heading("Bienvenido a Yummy", size="8"),
+                    rx.heading("Inicia sesión o registrate para comenzar", size="8"),
+                    rx.text(
+                        "Revisa el código de la aplicación ",
+                        rx.link(
+                            "aquí",
+                            href="https://github.com/reflex-dev/reflex-examples/tree/main/twitter",
+                        ),
+                        ".",
+                        color="gray",
+                        font_weight="medium",
+                    ),
+                    *args,
+                    style=styles.body_style,
                 ),
-                ".",
-                color="gray",
-                font_weight="medium",
-            ),
-            *args,
-            border_top_radius="10px",
-            box_shadow="0 4px 60px 0 rgba(0, 0, 0, 0.08), 0 4px 16px 0 rgba(0, 0, 0, 0.08)",
-            display="flex",
-            flex_direction="column",
-            align_items="center",
-            padding_top="36px",
-            padding_bottom="24px",
-            spacing="4",
+                
+                # border_top_radius="10px",
+                # box_shadow="0 4px 60px 0 rgba(0, 0, 0, 0.08), 0 4px 16px 0 rgba(0, 0, 0, 0.08)",
+                # display="flex",
+                # flex_direction="column",
+                # align_items="center",
+                # padding_top="36px",
+                # padding_bottom="24px",
+                spacing=styles.Size.DEFAULT.value,
+                style=styles.main_page_style
+            )
         )
